8d47d10e40883d67e330a5fe445a616e7f227e57
Jose Maria Flores Zazo <jmfloreszazo@gmail.com>
2022-11-22 09:21:31 +0100

    Sample
    Sample

---
+++ refvaluebenchmarknet6/TestBenchmark.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+[SimpleJob(RuntimeMoniker.Net60, baseline:true)]
+[SimpleJob(RuntimeMoniker.Net70)]
+
+//dotnet run -c Release -f net6.0 --runtimes net6.0 net7.0
+
+public class TestBenchmark
+{
+    private readonly PassValue _objetctPassValue = new();
+    private readonly PassRef _objetctPassref = new();
+
+    [Benchmark]
+    public void CallValue()
+    {
+        _objetctPassValue.SomeMethod();
+    }
+
+    [Benchmark]
+    public void CallRef()
+    {
+        _objetctPassref.SomeMethod();
+    }
+}
---
+++ refvaluebenchmarknet6/samples/PassValue.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+
+public class PassValue
+{
+    private static void SomePassValue1(string value)
+    {
+        SomePassValue2(value);
+    }
+
+    private static void SomePassValue2(string value)
+    {
+        SomePassValue3(value);
+    }
+
+    private static void SomePassValue3(string value)
+    {
+        SomePassValue4(value);
+    }
+
+    private static void SomePassValue4(string value)
+    {
+    }
+
+    private string LoremIpsum(int minWords, int maxWords,
+        int minSentences, int maxSentences,
+        int numParagraphs)
+    {
+        var words = new[]
+        {
+            "lorem", "ipsum", "dolor", "sit", "amet", "consectetuer",
+            "adipiscing", "elit", "sed", "diam", "nonummy", "nibh", "euismod",
+            "tincidunt", "ut", "laoreet", "dolore", "magna", "aliquam", "erat"
+        };
+
+        var rand = new Random();
+        var numSentences = rand.Next(maxSentences - minSentences)
+                           + minSentences + 1;
+        var numWords = rand.Next(maxWords - minWords) + minWords + 1;
+
+        var result = new StringBuilder();
+
+        for (var p = 0; p < numParagraphs; p++)
+        for (var s = 0; s < numSentences; s++)
+        {
+            for (var w = 0; w < numWords; w++)
+            {
+                if (w > 0) result.Append(" ");
+
+                result.Append(words[rand.Next(words.Length)]);
+            }
+
+            result.Append(". ");
+        }
+
+        return result.ToString();
+    }
+
+    public void SomeMethod()
+    {
+        var passMe = LoremIpsum(10, 10, 10, 10, 10);
+
+        for (var x = 0; x < 20; x++)
+        for (var i = 0; i < 50000000; i++)
+            SomePassValue1(passMe);
+    }
+}
