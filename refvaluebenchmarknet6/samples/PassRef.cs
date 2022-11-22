a5ea3467a43496945cc84b0f8a033fc6986b6b1f
Jose Maria Flores Zazo <jmfloreszazo@gmail.com>
2022-11-22 12:00:45 +0100

    Some refactor, more simple test
    Some refactor, more simple test

--- refvaluebenchmarknet6/samples/PassRef.cs
+++ refvaluebenchmarknet6/samples/PassRef.cs
@@ -2,64 +2,30 @@
 
 public class PassRef
 {
-    private string SomePassValue1(ref string value)
+    private int SomePassValue1(ref int value)
     {
         return SomePassValue2(ref value);
     }
 
-    private string SomePassValue2(ref string value)
+    private int SomePassValue2(ref int value)
     {
         return SomePassValue3(ref value);
     }
 
-    private string SomePassValue3(ref string value)
+    private int SomePassValue3(ref int value)
     {
         return SomePassValue4(ref value);
     }
 
-    private string SomePassValue4(ref string value)
+    private int SomePassValue4(ref int value)
     {
-        var result = "test";
+        var result = 2;
         return result;
     }
-
-    private string LoremIpsum(int minWords, int maxWords,
-        int minSentences, int maxSentences,
-        int numParagraphs)
-    {
-        var words = new[]
-        {
-            "lorem", "ipsum", "dolor", "sit", "amet", "consectetuer",
-            "adipiscing", "elit", "sed", "diam", "nonummy", "nibh", "euismod",
-            "tincidunt", "ut", "laoreet", "dolore", "magna", "aliquam", "erat"
-        };
-
-        var rand = new Random();
-        var numSentences = rand.Next(maxSentences - minSentences)
-                           + minSentences + 1;
-        var numWords = rand.Next(maxWords - minWords) + minWords + 1;
-
-        var result = new StringBuilder();
-
-        for (var p = 0; p < numParagraphs; p++)
-        for (var s = 0; s < numSentences; s++)
-        {
-            for (var w = 0; w < numWords; w++)
-            {
-                if (w > 0) result.Append(" ");
-
-                result.Append(words[rand.Next(words.Length)]);
-            }
-
-            result.Append(". ");
-        }
-
-        return result.ToString();
-    }
-
+    
     public void SomeMethod()
     {
-        var passMe = LoremIpsum(10, 10, 10, 10, 10);
+        var passMe = 1;
         for (var x = 0; x < 20; x++)
         for (var i = 0; i < 50000000; i++)
             SomePassValue1(ref passMe);
--- refvaluebenchmarknet6/samples/PassValue.cs
+++ refvaluebenchmarknet6/samples/PassValue.cs
@@ -2,64 +2,29 @@
 
 public class PassValue
 {
-    private string SomePassValue1(string value)
+    private int SomePassValue1(int value)
     {
         return SomePassValue2(value);
     }
 
-    private string SomePassValue2(string value)
+    private int SomePassValue2(int value)
     {
         return SomePassValue3(value);
     }
 
-    private string SomePassValue3(string value)
+    private int SomePassValue3(int value)
     {
         return SomePassValue4(value);
     }
 
-    private string SomePassValue4(string value)
+    private int SomePassValue4(int value)
     {
-        value = "test";
+        value = 2;
         return value;
     }
-
-    private string LoremIpsum(int minWords, int maxWords,
-        int minSentences, int maxSentences,
-        int numParagraphs)
-    {
-        var words = new[]
-        {
-            "lorem", "ipsum", "dolor", "sit", "amet", "consectetuer",
-            "adipiscing", "elit", "sed", "diam", "nonummy", "nibh", "euismod",
-            "tincidunt", "ut", "laoreet", "dolore", "magna", "aliquam", "erat"
-        };
-
-        var rand = new Random();
-        var numSentences = rand.Next(maxSentences - minSentences)
-                           + minSentences + 1;
-        var numWords = rand.Next(maxWords - minWords) + minWords + 1;
-
-        var result = new StringBuilder();
-
-        for (var p = 0; p < numParagraphs; p++)
-        for (var s = 0; s < numSentences; s++)
-        {
-            for (var w = 0; w < numWords; w++)
-            {
-                if (w > 0) result.Append(" ");
-
-                result.Append(words[rand.Next(words.Length)]);
-            }
-
-            result.Append(". ");
-        }
-
-        return result.ToString();
-    }
-
     public void SomeMethod()
     {
-        var passMe = LoremIpsum(10, 10, 10, 10, 10);
+        var passMe = 1;
 
         for (var x = 0; x < 20; x++)
         for (var i = 0; i < 50000000; i++)
